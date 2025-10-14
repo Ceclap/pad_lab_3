24cc4b437eb5e35545817aaedb9b544e88bddca7
Ceclap <cecleavictor@gmail.com>
2025-10-14 18:13:12 +0100

    Aloha

---
+++ server.py
@@ -0,0 +1,10 @@
+# Import bibliotecilor pentru pornirea serverului Web
+from http.server import HTTPServer, CGIHTTPRequestHandler
+# Adresa serverului și numărul portului pe care îl va 'asculta'
+server_address = ('', 80)
+# Cream obiectul server Web
+httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
+# Pornirea serverului Web
+print('pornire')
+httpd.serve_forever()
+print('oprire')
