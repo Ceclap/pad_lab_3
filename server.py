23d954d124e266fb916ce9b198eb16cf07469e51
Ceclap <cecleavictor@gmail.com>
2025-10-14 18:28:53 +0100

    CI/CD on

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 # Import bibliotecilor pentru pornirea serverului Web
 from http.server import HTTPServer, CGIHTTPRequestHandler
 # Adresa serverului și numărul portului pe care îl va 'asculta'
-server_address = ('', 80)
+server_address = ('', 8080)
 # Cream obiectul server Web
 httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
 # Pornirea serverului Web
